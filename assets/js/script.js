6c7b4b8ec3ca530c97653ce894f8f6637efbb3fe
alxrds <alxrds@hotmail.com>
2021-09-24 22:47:58 -0300

    correção

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ let lista = '';
 let num1 = '';
 let num2 = '';
 let calcList = [];
-let ultimaSoma = '';
+let ultimoCalculo = '';
 let calculadora = true;
 
 
@@ -24,8 +24,101 @@ let calculadora = true;
             }
     
             if(num1 && num2){
-                ultimaSoma = parseFloat(num1) + parseFloat(num2)
-                num1 = ultimaSoma;
+                ultimoCalculo = parseFloat(num1) + parseFloat(num2)
+                num1 = ultimoCalculo;
+                num2 = '';
+            }
+    
+            document.getElementById('valor').innerHTML = lista;
+            document.getElementById('display').innerHTML = '';
+
+            console.log('numero1: '+num1)
+            console.log('operação: '+ultimaOperacao)
+            console.log('numero2: '+num2)
+            console.log('total: '+ultimoCalculo)
+        
+        }
+    
+        if(btn == '-'){
+            valor = numero;
+            ultimaOperacao = btn;
+            lista += valor;
+    
+            if(!num1){
+                num1 = document.getElementById('display').innerHTML.substring(0, valor.length -1);
+            }else{
+                num2 = document.getElementById('display').innerHTML.substring(0, valor.length -1);
+            }
+    
+            if(num1 && num2){
+                ultimoCalculo = parseFloat(num1) - parseFloat(num2)
+                num1 = ultimoCalculo;
+                num2 = '';
+            }
+    
+            document.getElementById('valor').innerHTML = lista;
+            document.getElementById('display').innerHTML = '';
+        
+        }
+
+        if(btn == '*'){
+            valor = numero;
+            ultimaOperacao = btn;
+            lista += valor;
+    
+            if(!num1){
+                num1 = document.getElementById('display').innerHTML.substring(0, valor.length -1);
+            }else{
+                num2 = document.getElementById('display').innerHTML.substring(0, valor.length -1);
+            }
+    
+            if(num1 && num2){
+                ultimoCalculo = parseFloat(num1) * parseFloat(num2)
+                num1 = ultimoCalculo;
+                num2 = '';
+            }
+    
+            document.getElementById('valor').innerHTML = lista;
+            document.getElementById('display').innerHTML = '';
+        
+        }
+
+        if(btn == '/'){
+            valor = numero;
+            ultimaOperacao = btn;
+            lista += valor;
+    
+            if(!num1){
+                num1 = document.getElementById('display').innerHTML.substring(0, valor.length -1);
+            }else{
+                num2 = document.getElementById('display').innerHTML.substring(0, valor.length -1);
+            }
+    
+            if(num1 && num2){
+                ultimoCalculo = parseFloat(num1) / parseFloat(num2)
+                num1 = ultimoCalculo;
+                num2 = '';
+            }
+    
+            document.getElementById('valor').innerHTML = lista;
+            document.getElementById('display').innerHTML = '';
+        
+        }
+
+        if(btn == '%'){
+            valor = numero;
+            ultimaOperacao = btn;
+            lista += valor;
+    
+            if(!num1){
+                num1 = document.getElementById('display').innerHTML.substring(0, valor.length -1);
+            }else{
+                num2 = document.getElementById('display').innerHTML.substring(0, valor.length -1);
+            }
+    
+            if(num1 && num2){
+                ultimoCalculo = (parseFloat(num2) - parseFloat(num1)) * num1 / 100;
+                num1 = ultimoCalculo;
                 num2 = '';
             }
     
@@ -34,40 +127,7 @@ let calculadora = true;
         
         }
     
-        if(btn == '-'){
-            valor = numero;
-            ultimaOperacao = btn;
-            lista += valor;
-            document.getElementById('valor').innerHTML = lista;
-            document.getElementById('display').innerHTML = '';
-        }
-        if(btn == '*'){
-            valor = numero;
-            ultimaOperacao = btn;
-            lista += valor;
-            document.getElementById('valor').innerHTML = lista;
-            document.getElementById('display').innerHTML = '';
-        }
-        if(btn == '/'){
-            valor = numero;
-            ultimaOperacao = btn;
-            lista += valor;
-            document.getElementById('valor').innerHTML = lista;
-            document.getElementById('display').innerHTML = '';
-        }
-        if(btn == '%'){
-            valor = numero;
-            ultimaOperacao = btn;
-            lista += valor;
-            document.getElementById('valor').innerHTML = lista;
-            document.getElementById('display').innerHTML = '';
-        }
-    
     }
-
-
-
-
 
 function limpar(){
     window.location.reload();
@@ -90,8 +150,25 @@ function calcular(){
         if(ultimaOperacao == '+'){
             document.getElementById('display').innerHTML = parseFloat(num1) + parseFloat(num2);
         }
+
+        if(ultimaOperacao == '-'){
+            document.getElementById('display').innerHTML = parseFloat(num1) - parseFloat(num2);
+        }
+
+        if(ultimaOperacao == '*'){
+            document.getElementById('display').innerHTML = parseFloat(num1) * parseFloat(num2);
+        }
+
+        if(ultimaOperacao == '/'){
+            document.getElementById('display').innerHTML = parseFloat(num1) / parseFloat(num2);
+        }
     }
 
     calculadora = false;
+
+    console.log('numero1: '+num1)
+    console.log('operação: '+ultimaOperacao)
+    console.log('numero2: '+num2)
+    console.log('total: '+ultimoCalculo)
 
 }
